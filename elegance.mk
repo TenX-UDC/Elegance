e437df0527ab774df45bb88d58b71ae7d408da12
Aston-Martinn <advaithbhat9@gmail.com>
2024-08-15 17:22:43 +0530

    Elegance: Apply monet accurate shade on settings [3/3]
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- elegance.mk
+++ elegance.mk
@@ -22,7 +22,8 @@ PRODUCT_PACKAGES += \
 # Accurate shade
 PRODUCT_PACKAGES += \
     AccurateShadeOverlayAndroid \
-    AccurateShadeOverlaySystemUI
+    AccurateShadeOverlaySystemUI \
+    AccurateShadeOverlaySettings
 
 # Fonts
 PRODUCT_PACKAGES += \
